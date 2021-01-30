c9f7368bb51e8dc82a96b7a25cb25f128a9bed93
Alonso-007 <alonso.afs@gmail.com>
2021-01-30 19:02:55 -0300

    REST testado

---
+++ CursoMc/Controllers/CategoriasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<String> Listar()
+        {
+            return Ok("REST está funcionando");
+        }
+    }
+}
