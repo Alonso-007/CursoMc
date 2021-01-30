9e45890fc731cddabd719885f0289b02742b09a0
Alonso-007 <alonso.afs@gmail.com>
2021-01-30 19:28:08 -0300

    Testando a primeira classe de dominio

--- CursoMc/Controllers/CategoriasController.cs
+++ CursoMc/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CursoMc.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,25 @@ namespace CursoMc.Controllers
     public class CategoriasController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<String> Listar()
+        public ActionResult<IEnumerable<Categoria>> Listar()
         {
-            return Ok("REST está funcionando");
+            Categoria cat1 = new Categoria()
+            {
+                Id = 1,
+                Nome = "Informática"
+            };
+
+            Categoria cat2 = new Categoria()
+            {
+                Id = 2,
+                Nome = "Escritório"
+            };
+
+            List<Categoria> lista = new List<Categoria>();
+            lista.Add(cat1);
+            lista.Add(cat2);
+
+            return Ok(lista);
         }
     }
 }
---
+++ CursoMc/Model/Categoria.cs
@@ -0,0 +1,38 @@
+﻿namespace CursoMc.Model
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public override int GetHashCode()
+        {
+            int prime = 31;
+            int result = 1;
+            result = prime * result + Id.GetHashCode();
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            
+            Categoria other = (Categoria)obj;
+            if (Id == 0)
+            {
+                if (other.Id != 0)
+                {
+                    return false;
+                }
+                else if (!Id.Equals(other.Id))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
